57adcf6aff8b5c1b37fc45372c3ee9854b804aa4
linhaishe <42030043+linhaishe@users.noreply.github.com>
2023-04-18 23:02:04 +0800

    feat: 将留言显示在留言板区域内

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,41 @@
-console.log("we are here");
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+
 const socket = io();
 // server emit the message and client side can receive the message
 socket.on("message", (messsage) => {
   console.log(messsage);
+  outputMessage(messsage);
+
+  // scroll down 发送消息之后，自动下拉到可视范围内
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
+
+// Message submit
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Get message text
+  let msg = e.target.elements.msg.value;
+
+  msg = msg.trim();
+
+  if (!msg) {
+    return false;
+  }
+
+  // Emit message to server
+  socket.emit("chatMessage", msg);
+
+  // Clear input
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
+
+// Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">Mary<span>9:15pm</span></p><p class="text">${message}</p>`;
+  document.querySelector(".chat-messages").appendChild(div);
+}
--- server.js
+++ server.js
@@ -23,6 +23,11 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     io.emit("message", "A user has left chat");
   });
+
+  // listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", msg);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
